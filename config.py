5f059420464f5d2ab97a9fed8e6d23d1b0d2e06c
张奕伟 <zhangyiwei@zhangyiweideMacBook-Air.local>
2017-11-24 01:50:35 +0800

    mod config

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN=True
-	FLASKY_POSTS_PER_PAGE=20
-	FLASKY_COMMENTS_PER_PAGE=20
+	FLASKY_POSTS_PER_PAGE=10
+	FLASKY_COMMENTS_PER_PAGE=10
+	FLASKY_USERS_PER_PAGE=20
 	FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
 	FLASKY_MAIL_SENDER=os.environ.get('FLASKY_MAIL_SENDER')
 	FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
